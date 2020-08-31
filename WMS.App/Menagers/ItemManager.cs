dda3ca9a1d92b61b0a764a37151c1ba52b15c6f7
Monika Zając <monikaannazajac@gmail.com>
2020-08-31 23:39:21 +0200

    Testing

---
+++ WMS.App/Menagers/ItemManager.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using WMS.App.Concrete;
+using WMS.Domain;
+
+namespace WMS.App.Menagers
+{
+    public class ItemManager
+    {
+        private ItemService _itemService;
+        private CategoryService _categoryService;
+        public ItemManager(ItemService itemService, CategoryService categoryService)
+        {
+            _itemService = itemService;
+            _categoryService = categoryService;
+        }
+        public void AddItem()
+        {
+            Console.WriteLine("What is category of item which you want to add?");
+            string nameCat = Console.ReadLine();
+            bool beExistCategory = _categoryService.Existed(nameCat);
+            if (beExistCategory)
+            {
+                Console.WriteLine("What item do you want to add?");
+                string nameItem = Console.ReadLine();
+                bool beExist = _itemService.Existed(nameItem);
+                Console.WriteLine("How many items do you want to add?");
+                int.TryParse(Console.ReadLine(), out int number);
+                if (beExist)
+                {
+                    _itemService.AddQuantity(nameItem, number);
+                    Console.WriteLine($"You add {number} {nameItem}`s.");
+                    
+                }
+                else
+                {
+                    int idCat = _categoryService.GetCatId(nameCat);
+                    int itemId = _itemService.GetLastId();
+                    Item item = new Item(idCat, nameCat, itemId+1, nameItem, number);
+                    _itemService.AddItem(item);
+                    Console.WriteLine($"You add {number} {nameItem}`s.");
+                }
+            }
+            else
+            {
+                int catId = _categoryService.GetLastId();
+                Category category = new Category(catId+1, nameCat);
+                _categoryService.AddItem(category);
+                Console.WriteLine("What item do you want to add?");
+                string nameItem = Console.ReadLine();
+                Console.WriteLine("How many items do you want to add?");
+                int.TryParse(Console.ReadLine(), out int number);
+                int itemId = _itemService.GetLastId();
+                Item item = new Item(catId+1, nameCat, itemId+1, nameItem, number);
+                _itemService.AddItem(item);
+                Console.WriteLine($"You add {number} {nameItem}`s.");
+            }
+        }
+        public void RemoveItem()
+        {
+            Console.WriteLine("What item do you want to remove?");
+            string nameItem = Console.ReadLine();
+            bool beExist = _itemService.Existed(nameItem);
+            if (beExist)
+            {
+                Console.WriteLine("How many items do you want to remove?");
+                int.TryParse(Console.ReadLine(), out int number);
+                int quantity=_itemService.RemoveQuantity(nameItem, number);
+                if(quantity>=number)
+                {
+                    Console.WriteLine($"You removed {number} {nameItem}");
+                    if(quantity==number)
+                    {
+                        Console.WriteLine($"The {nameItem} is out of stock now");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, you want remove too many items. You don`t have it on stock");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Sorry, it`s impossible. You don`t have {nameItem} on stock");
+            }
+        }
+        public void Availability()
+        {
+            Console.WriteLine("What availability of product do you want to check?");
+            string nameItem = Console.ReadLine();
+            bool beExist = _itemService.Existed(nameItem);
+            if (beExist)
+            {
+                int quantity = _itemService.GetQuantity(nameItem);
+                Console.WriteLine("The quantity of product is: " + quantity);
+            }
+            else
+            {
+                Console.WriteLine("Sorry, this product is out of stock");
+            }
+        }
+        public void IsLowLevel()
+        {
+            foreach(var i in _itemService.IsLow())
+            {
+                Console.WriteLine($"The {i.Name}");
+            }    
+            
+        }
+        public void ShowItems()
+        {
+            int a = 1;
+            foreach(var i in _itemService.GetAll())
+            {
+                Console.WriteLine($"{a}. {i}");
+                a++;
+            }
+        }
+        public void ShowCategories()
+        {
+            int a = 1;
+            foreach (var i in _categoryService.GetAll())
+            {
+                Console.WriteLine($"{a}. {i}");
+                a++;
+            }
+        }
+    }
+}
